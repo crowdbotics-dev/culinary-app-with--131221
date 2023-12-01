28926e92c4c7badf85e447229d17fc8813c44199
Crowdbotics <engineering@crowdbotics.com>
2023-12-01 10:29:48 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "culinary-app-with--131221",
+  url: "https://culinary-app-with--131221.botics.co",
+  api: "https://culinary-app-with--131221.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
